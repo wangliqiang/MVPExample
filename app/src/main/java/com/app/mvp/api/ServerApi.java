5172efa0e096c89c5fe8721d79e75a43e9db405f
王立强 <wangliqiangorz@163.com>
2016-10-17 12:00:04 +0800

    update

--- app/src/main/java/com/app/mvp/api/ServerApi.java
+++ app/src/main/java/com/app/mvp/api/ServerApi.java
@@ -3,8 +3,8 @@ package com.app.mvp.api;
 public class ServerApi {
 
     public static String BASEURL = "https://api.douban.com/v2/movie/";
-
+    //豆瓣电影TOP250
     public static String TOP250 = BASEURL + "top250";
-    
+    //电影详情
     public static String MOVIEDETAIL = BASEURL + "subject/";
 }
