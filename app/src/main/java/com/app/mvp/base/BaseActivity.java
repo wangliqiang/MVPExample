d1388a0b7d981cf801faa014c04027bd64e6eb30
王立强 <wangliqiangorz@163.com>
2016-10-17 16:43:27 +0800

    update

--- app/src/main/java/com/app/mvp/base/BaseActivity.java
+++ app/src/main/java/com/app/mvp/base/BaseActivity.java
@@ -1,7 +1,9 @@
 package com.app.mvp.base;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import com.nispok.snackbar.Snackbar;
 import com.nispok.snackbar.SnackbarManager;
@@ -28,4 +30,22 @@ public abstract class BaseActivity extends AppCompatActivity{
     public void showError() {
         SnackbarManager.show(Snackbar.with(this).text("暂无数据").duration(Snackbar.SnackbarDuration.LENGTH_SHORT));
     }
+
+//    private ProgressDialog dialog;
+//
+//    public void showLoading() {
+//        if (dialog != null && dialog.isShowing()) return;
+//        dialog = new ProgressDialog(this);
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        dialog.setCanceledOnTouchOutside(false);
+//        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//        dialog.setMessage("请求数据中...");
+//        dialog.show();
+//    }
+//
+//    public void dismissLoading() {
+//        if (dialog != null && dialog.isShowing()) {
+//            dialog.dismiss();
+//        }
+//    }
 }
