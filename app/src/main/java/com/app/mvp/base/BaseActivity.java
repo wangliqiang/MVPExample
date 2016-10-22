783e96361689fcb0c406ad251330b5530b747de9
王立强 <wangliqiangorz@163.com>
2016-10-22 16:50:04 +0800

    update

--- app/src/main/java/com/app/mvp/base/BaseActivity.java
+++ app/src/main/java/com/app/mvp/base/BaseActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
+import com.lzy.okgo.OkGo;
 import com.nispok.snackbar.Snackbar;
 import com.nispok.snackbar.SnackbarManager;
 
@@ -22,30 +23,43 @@ public abstract class BaseActivity extends AppCompatActivity{
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        OkGo.getInstance().cancelTag(this);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         ButterKnife.unbind(this);
+        OkGo.getInstance().cancelTag(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        OkGo.getInstance().cancelTag(this);
     }
 
     public void showError() {
         SnackbarManager.show(Snackbar.with(this).text("暂无数据").duration(Snackbar.SnackbarDuration.LENGTH_SHORT));
     }
 
-//    private ProgressDialog dialog;
-//
-//    public void showLoading() {
-//        if (dialog != null && dialog.isShowing()) return;
-//        dialog = new ProgressDialog(this);
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        dialog.setCanceledOnTouchOutside(false);
-//        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-//        dialog.setMessage("请求数据中...");
-//        dialog.show();
-//    }
-//
-//    public void dismissLoading() {
-//        if (dialog != null && dialog.isShowing()) {
-//            dialog.dismiss();
-//        }
-//    }
+    private ProgressDialog dialog;
+
+    public void showLoading() {
+        if (dialog != null && dialog.isShowing()) return;
+        dialog = new ProgressDialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setMessage("请求数据中...");
+        dialog.show();
+    }
+
+    public void dismissLoading() {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+        }
+    }
 }
