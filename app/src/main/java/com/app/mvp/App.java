1d452ad7e335d5eb775bc91b39a929d99dda32e9
王立强 <wangliqiangorz@163.com>
2017-01-05 11:19:53 +0800

    add Timber

--- app/src/main/java/com/app/mvp/App.java
+++ app/src/main/java/com/app/mvp/App.java
@@ -1,14 +1,17 @@
 package com.app.mvp;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.app.mvp.utils.FakeCrashLibrary;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.cache.CacheEntity;
 import com.lzy.okgo.cache.CacheMode;
-import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
 import com.squareup.leakcanary.LeakCanary;
 
 import java.util.logging.Level;
+
+import timber.log.Timber;
 
 /**
  * Created by 王立强 on 2016/10/9.
@@ -19,20 +22,30 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        /**
+         * 初始化Timber
+         */
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return;
         }
+        /**
+         * 初始化LeakCanary
+         */
         LeakCanary.install(this);
-
-        //初始化
+        /**
+         * 初始化OkGo
+         */
         OkGo.init(this);
-
         try {
             //全局参数设置
             OkGo.getInstance()
                     //打开调试开关
-                    .debug("OkGo", Level.INFO, true)
+                    .debug("OkGo", Level.SEVERE, true)
                     //设置超时时间，默认60秒
                     .setConnectTimeout(OkGo.DEFAULT_MILLISECONDS)
                     .setReadTimeOut(OkGo.DEFAULT_MILLISECONDS)
@@ -43,6 +56,25 @@ public class App extends Application {
                     .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /** A tree which logs important information for crash reporting. */
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            FakeCrashLibrary.log(priority, tag, message);
+
+            if (t != null) {
+                if (priority == Log.ERROR) {
+                    FakeCrashLibrary.logError(t);
+                } else if (priority == Log.WARN) {
+                    FakeCrashLibrary.logWarning(t);
+                }
+            }
         }
     }
 }
